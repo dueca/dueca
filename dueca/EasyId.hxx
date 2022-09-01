888c2e2baacc330512096db3bc42a057064d1036
René van Paassen <rene.vanpaassen@gmail.com>
2022-09-01 20:41:09 +0200

    make EasyId accessible to client code

--- dueca/EasyId.hxx
+++ dueca/EasyId.hxx
@@ -1,12 +1,12 @@
 /* ------------------------------------------------------------------   */
 /*      item            : EasyId.hxx
         made by         : Rene van Paassen
-        date            : 061215
+        date            : 220700
         category        : header file
         description     :
-        changes         : 061215 first version
+        changes         : ?? first version
         language        : C++
-        copyright       : (c) 2016 TUDelft-AE-C&S
+        api             : DUECA_API
         copyright       : (c) 2022 René van Paassen
         license         : EUPL-1.2
 */
@@ -21,14 +21,21 @@
 
 DUECA_NS_START
 
-/** Some objects need an id, but cannot derive from NamedObject,
-    because they could be initialized automatically before the
-    ObjectManager. A pointer to an EasyId can be used.
+/** Internally in DUECA, some objects need an id, but cannot derive
+    from NamedObject, because they could be initialized automatically
+    before the ObjectManager. A pointer to an EasyId can then be used,
+    and the EsyId is instantiated later.
 
-    In rare cases, this can also be used in application code, however,
-    for your own sake, avoid mis-using this; to give NamedObject
-    capabilities to helper classes (e.g., for opening a channel,
-    activities, callback), better use dueca::AssociateObject.
+    In some cases, having a class that derives from EasyId can be
+    useful in application code. Only do this when you have an object
+    that is not a Module, and also not associated with a (single)
+    Module, but that still needs some Module-like capabilities, such
+    as accessing a channel.
+
+    When having classes that are clearly associated with a
+    dueca::Module (helper classes to handle a specific channel entry
+    or the like), instead use dueca::AssociateObject, which can
+    "borrow" access to the Module's name and identity.
 */
 class EasyId: public NamedObject
 {
